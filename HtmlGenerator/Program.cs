fa9366db1ccb681c7a69d6b2059a6ee5cad48813
Greta <greta@local>
2020-03-07 19:09:33 +0200

    Generate new solution

---
+++ HtmlGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HtmlGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
